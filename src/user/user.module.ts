0c7395d3ae733d03dfc2f3ededed34f0b51c83e1
tatyana.lebedeva <tslebedeva3103@gmail.com>
2026-04-12 23:46:42 +0200

    feat: remove forward ref

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,8 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { ArticleModule } from '../article/article.module';
-import { CommentModule } from '../comment/comment.module';
 
 @Module({
-  imports: [forwardRef(() => ArticleModule), forwardRef(() => CommentModule)],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
